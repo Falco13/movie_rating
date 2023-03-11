d7801911727b3103dadfcfe60b989e4ccff56931
Falco13 <keanu0608@gmail.com>
2023-03-12 00:27:57 +0200

    admin site added

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from accounts.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ['username',
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'date_of_birth',
+                    'is_staff',
+                    'is_superuser',
+                    'date_joined',
+                    'last_login',
+                    ]
--- movie_rating/admin.py
+++ movie_rating/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from movie_rating.models import Movie, Rating
 
-# Register your models here.
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'description', 'year', 'country', 'is_active']
+    prepopulated_fields = {'slug': ('title',)}
+    # formfield_overrides = {
+    #     models.CharField: {'widget': TextInput(attrs={'size': '20'})},
+    #     models.TextField: {'widget': Textarea(attrs={'rows': 4, 'cols': 40})},
+    # }
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ['movie', 'user', 'created_date', 'rating', 'is_active']
+    # readonly_fields = ['movie', 'user', 'created_date', 'rating']
+    list_editable = ['is_active']
