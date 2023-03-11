ef0c3af02f99625a3c4b91674ef921f0dacafc34
Falco13 <keanu0608@gmail.com>
2023-03-11 15:45:04 +0200

    start

---
+++ movie_rating/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=55, unique=True)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    year = models.PositiveSmallIntegerField()
+    country = models.CharField(max_length=20)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+
+class Rating(models.Model):
+    RATE_CHOICES = (
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+        (6, 6),
+        (7, 7),
+        (8, 8),
+        (9, 9),
+        (10, 10),
+    )
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='rating')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    rating = models.PositiveSmallIntegerField(choices=RATE_CHOICES, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'Movie: {self.movie} is rated {self.rating}'
