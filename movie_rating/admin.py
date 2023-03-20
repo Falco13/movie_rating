774700dd856971f6ed0df58ef53590afe9dabcad
Falco13 <keanu0608@gmail.com>
2023-03-20 14:55:25 +0200

    description_short for admin site

--- movie_rating/admin.py
+++ movie_rating/admin.py
@@ -4,16 +4,17 @@ from movie_rating.models import Movie, Rating
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'description', 'year', 'country', 'is_active']
+    list_display = ['title', 'slug', 'description_short', 'year', 'country', 'is_active']
     prepopulated_fields = {'slug': ('title',)}
-    # formfield_overrides = {
-    #     models.CharField: {'widget': TextInput(attrs={'size': '20'})},
-    #     models.TextField: {'widget': Textarea(attrs={'rows': 4, 'cols': 40})},
-    # }
+
+    @staticmethod
+    def description_short(obj: Movie):
+        if len(obj.description) < 50:
+            return obj.description
+        return obj.description[:50] + '...'
 
 
 @admin.register(Rating)
 class RatingAdmin(admin.ModelAdmin):
     list_display = ['movie', 'user', 'created_date', 'rating', 'is_active']
-    # readonly_fields = ['movie', 'user', 'created_date', 'rating']
     list_editable = ['is_active']
