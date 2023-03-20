9a527f23b4730ac8536b68e25d75ab532d8668c1
Falco13 <keanu0608@gmail.com>
2023-03-20 15:27:25 +0200

    admin ratings, select_related

--- movie_rating/admin.py
+++ movie_rating/admin.py
@@ -4,7 +4,7 @@ from movie_rating.models import Movie, Rating
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'description_short', 'year', 'country', 'is_active']
+    list_display = ['title', 'slug', 'description_short', 'year', 'country', 'is_active', 'rating_short']
     prepopulated_fields = {'slug': ('title',)}
 
     @staticmethod
@@ -13,8 +13,17 @@ class MovieAdmin(admin.ModelAdmin):
             return obj.description
         return obj.description[:50] + '...'
 
+    @staticmethod
+    def rating_short(obj: Movie):
+        if len(str(obj.average_rating)) < 3:
+            return str(obj.average_rating)
+        return str(obj.average_rating)[:3]
+
 
 @admin.register(Rating)
 class RatingAdmin(admin.ModelAdmin):
     list_display = ['movie', 'user', 'created_date', 'rating', 'is_active']
     list_editable = ['is_active']
+
+    def get_queryset(self, request):
+        return Rating.objects.select_related('movie', 'user').all()
