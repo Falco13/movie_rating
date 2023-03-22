e2f464ccafaf5bee27437136705d86a9afa4c231
Falco13 <keanu0608@gmail.com>
2023-03-22 15:25:46 +0200

    added user age in model and template

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -5,3 +6,9 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name='email')
     date_of_birth = models.DateField(blank=True, null=True, verbose_name='date of birth')
+
+    def age(self):
+        if self.date_of_birth:
+            return int((datetime.date.today() - self.date_of_birth).days / 365.25)
+        else:
+            return None
