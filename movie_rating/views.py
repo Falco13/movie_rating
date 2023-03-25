593ebfc73f3d876ff08ce0e7c314de6700b864c8
Falco13 <keanu0608@gmail.com>
2023-03-25 14:05:30 +0200

    user can always change his rating

--- movie_rating/views.py
+++ movie_rating/views.py
@@ -28,11 +28,9 @@ def detail_movie(request, slug):
         form = RatingForm(request.POST)
         if form.is_valid():
             rating = request.POST.get('rating')
-            if Rating.objects.filter(user=request.user, movie=movie).exists():
-                messages.error(request, 'You have already rated this movie!')
-            else:
-                Rating.objects.create(user=request.user, rating=rating, movie=movie)
-                messages.success(request, 'Thank you for your review of the movie!')
+            Rating.objects.filter(user=request.user, movie=movie).delete()
+            Rating.objects.create(user=request.user, rating=rating, movie=movie)
+            messages.success(request, 'Thank you for your review of the movie!')
             return HttpResponseRedirect(reverse('detail', args=(slug,)))
     else:
         form = RatingForm()
